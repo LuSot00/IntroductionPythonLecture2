d38c06158559031e468bb3f20899ede627d2f10d
LyubovS <l.sotskova@gmail.com>
2023-11-09 20:56:26 +0300

    Add new simbol

--- Example1.py
+++ Example1.py
@@ -1,7 +1,13 @@
-list_1 = [1, 5]
+# list_1 = [1, 5]
 
+# print(list_1)
+
+# list_1.append(8)
+
+# print(list_1)
+
+list_1 = [] # Добавление элементов в лист
 print(list_1)
-
-list_1.append(8)
-
-print(list_1)
+for i in range(5):
+    list_1.append(i)
+    print(list_1)
