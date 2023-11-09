1301a200dde29412ec805bbaac7be351c2159d66
LyubovS <l.sotskova@gmail.com>
2023-11-09 20:59:41 +0300

    Delite simbol

--- Example1.py
+++ Example1.py
@@ -6,8 +6,17 @@
 
 # print(list_1)
 
-list_1 = [] # Добавление элементов в лист
-print(list_1)
-for i in range(5):
-    list_1.append(i)
-    print(list_1)
+# list_1 = [] # Добавление элементов в лист
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1)
+
+# Удаление последнего элемента
+list_1 = [12, 7, -1, 21, 0]
+print(list_1.pop()) # 0
+print(list_1) # [12, 7, -1, 21]
+print(list_1.pop()) # 21
+print(list_1) # [12, 7, -1]
+print(list_1.pop()) # -1
+print(list_1) # [12, 7]
